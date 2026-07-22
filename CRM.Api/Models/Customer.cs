924c96d70ea3130c988b0aae7ddbd5a8c4d08c28
Salvatore Allegra <salallegra@gmail.com>
2026-07-21 17:16:52 -0700

    Add customer model

---
+++ CRM.Api/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRM.Api.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [MaxLength(100)]
+    public string LastName { get; set; } = string.Empty;
+
+    [EmailAddress]
+    [MaxLength(255)]
+    public string? Email { get; set; }
+
+    [Phone]
+    [MaxLength(50)]
+    public string? Phone { get; set; }
+
+    public DateTime CreatedAtUtc { get; set; }
+}
